01879fed6d5e2deb307e50741d00ef5e958670c8
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-23 19:48:49 +0500

    asdasd

--- resources/views/frontend/showproduct.blade.php
+++ resources/views/frontend/showproduct.blade.php
@@ -1,5 +1,5 @@
 <div class="col-xl-3 col-lg-4 col-md-6 ">
-    <div class="product__item wow fadeInUp animated" data-wow-duration="1500ms" data-wow-delay="000ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInUp;">
+    <div class="product__item">
         <div class="product__item__image">
             @if(isset($color))
             @if(DB::table('product_variation_images')->where('color' , $color)->where('product_id' , $r->id)->first())
@@ -18,18 +18,7 @@
         <div class="product__item__content">
             <h4 class="product__item__title"><a href="{{route('product-detail',$r->slug)}}">{{ $r->title }}</a></h4>
             @if($r->price != 0)
-            <div class="product__item__price">Starting From ${{ DB::table('product_colors')->where('product_id' , $r->id)->first()->price }}</div>
-            @endif
-            @if($r->cat_id == 6)
-            <a href="{{route('product-detail',$r->slug)}}" class="floens-btn product__item__link">
-                <span>ENQUIRE Now</span>
-                <i class="fa fa-question-circle"></i>
-            </a>
-            @else
-            <a href="{{route('product-detail',$r->slug)}}" class="floens-btn product__item__link">
-                <span>Add to Cart</span>
-                <i class="fa fa-shopping-cart"></i>
-            </a>
+            <a class="product__item__price">Starting From ${{ DB::table('product_colors')->where('product_id' , $r->id)->first()->price }}</a>
             @endif
         </div>
     </div>

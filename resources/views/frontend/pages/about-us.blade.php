e5363e05eb04471cff4460c6066d81f5e791152a
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-05-21 17:15:46 +0500

    asdsad

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -19,12 +19,27 @@
         <div class="row gutter-y-60">
             <div class="col-lg-12">
                 <div class="about-one__content">
-                    <div class="sec-title sec-title--border" style="display:block;">
-                        <h4>Our Story</h4>
-                        <h3 class="sec-title__title">Crafting Memories, Weaving Dreams.</h3><!-- /.sec-title__title -->
-                    </div><!-- /.sec-title -->
                     <div class="about-one__content__text wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="00ms">
-                        <p class="about-one__text">At CARGOZ, we're passionate about transforming spaces into beautiful havens with our exquisite collection of carpets and rugs. Founded with a vision to redefine home decor, we curate timeless pieces that blend craftsmanship, elegance, and comfort. With years of experience in the industry, we understand the transformative power of quality rugs, and our journey is driven by a commitment to bring exceptional designs to every home.</p><!-- /.about-one__text -->
+                        <p class="about-one__text">Welcome to our world of exquisite hand-woven rugs and carpets! At Jagkris Home Australia, we take pride in bringing you a diverse selection of handcrafted rugs and broadloom carpets, meticulously sourced from responsible manufacturers. Each piece in our collection is &quot;GoodWeave&quot; certified, ensuring ethical production and supporting fair labor practices.</p>
+                        <p class="about-one__text mt-5">We connect with one of the oldest manufacturing establishments in India, where our team
+carefully researches and designs each rug using contemporary weaving techniques such as
+hand-tufted, hand loom, punja, and pit loom. Whether you&#39;re seeking to enhance your
+home with the timeless beauty of a hand-woven rug or the elegance of a broadloom carpet,
+our extensive stock caters to every style and need. Explore our range and discover the
+perfect addition to your space.</p>
+                        <p class="about-one__text mt-5">At the heart of our company are the core values of providing authentic products and
+maintaining honest trade practices. Our mission is to bridge the gap between the hand-
+woven industry and our valued customers by offering genuine insights into the intricate
+making process. We are dedicated to educating our customers and providing a vast variety
+of rugs to choose from, ensuring you find the perfect piece for your home.</p>
+<p class="about-one__text mt-5">To cater to your unique design needs, we offer bespoke custom design services. Whether
+you have a specific vision in mind or need assistance in creating a design that complements
+your home decor, our expert team is here to help. From selecting the perfect materials to
+finalizing the intricate details, we work closely with you to bring your vision to life, resulting
+in a one-of-a-kind rug that is truly yours</p>
+<p class="about-one__text mt-5">We provide the most authentic information on rug manufacturing processes, offering you
+honest and detailed insights to guide your rug selection journey. Our expertise ensures you
+make informed decisions, choosing the perfect rug that fits your style and needs.</p>
                     </div><!-- /.about-one__content__text -->
                 </div><!-- /.about-one__content -->
             </div><!-- /.col-lg-6 -->
